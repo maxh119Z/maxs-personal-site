892514787ece7c9ccf2028222d4d479dd76c924a
Max Zhang <142354778+maxh119Z@users.noreply.github.com>
2026-06-05 11:32:07 -0700

    more

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import Header from "./components/header";
 import Footer from "./components/footer";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
