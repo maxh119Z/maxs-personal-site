9898126af2f5a49ec49d037cb8447443ffb17962
Max Zhang <142354778+maxh119Z@users.noreply.github.com>
2026-06-05 11:42:01 -0700

    website

--- app/layout.tsx
+++ app/layout.tsx
@@ -55,7 +55,8 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}
-
+         <Analytics />
+         <SpeedInsights></SpeedInsights>
 
 
         <script
