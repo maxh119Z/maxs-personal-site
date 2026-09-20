92719c03c8c99eece0df080031c4bf0e90193bd6
Max Zhang <142354778+maxh119Z@users.noreply.github.com>
2026-09-20 16:24:49 -0700

    new quotes

--- app/components/quotes.ts
+++ app/components/quotes.ts
@@ -14,7 +14,7 @@ export const QUOTES: Quote[] = [
         attribution: "Nicholas Carr",
     },
     {
-        quote: "Standing on the bare ground — my head bathed by the blithe air, and uplifted into infinite space — all mean egotism vanishes. I become a transparent eye-ball; I am nothing; I see all; the currents of the Universal Being circulate through me; I am part or particle of God.",
+        quote: "Standing on the bare ground — my head bathed by the blithe air, and uplifted into infinite space — all mean egotism vanishes. I become a transparent eye-ball; I am nothing; I see all",
         attribution: "Ralph Waldo Emerson",
     },
     {
@@ -33,13 +33,13 @@ export const QUOTES: Quote[] = [
         quote: "Clementine: ‘What do we do?’ Joel: ‘Enjoy it.’",
         attribution: "Eternal Sunshine of the Spotless Mind",
     },
-    {
-        quote: "Maybe life has no equal trade, maybe you can give up all you got, and get nothing back. Even if I can't prove it's true, I have to try",
-        attribution: "Edward Elric",
-    },
+    // {
+    //     quote: "Maybe life has no equal trade, maybe you can give up all you got, and get nothing back. Even if I can't prove it's true, I have to try",
+    //     attribution: "Edward Elric",
+    // },
     {
         quote: "'It hasn't even been 1/100th of my life.' 'It's interesting, isn't it? That this 1/100th changed you.'",
-        attribution: "Frieren, then Eisen",
+        attribution: "Frieren and Eisen",
     },
     // {
     //     quote: "Great Scott!",
@@ -53,26 +53,26 @@ export const QUOTES: Quote[] = [
         quote: "Are we also losing our capacity for rage?",
         attribution: "Byung-Chul Han",
     },
-    {
-        quote: "What? This isn't true! I didn't write this! Mima's Ghost: Of course you didn't. The real Mima is writing this.",
-        attribution: "Perfect Blue",
-    },
+    // {
+    //     quote: "What? This isn't true! I didn't write this! Mima's Ghost: Of course you didn't. The real Mima is writing this.",
+    //     attribution: "Perfect Blue",
+    // },
     // {
     //     quote: "You mustn't be afraid to dream a little bigger, darling.",
     //     attribution: "Inception",
     // },
+    // {
+    //     quote: "You teach best what you most need to learn.",
+    //     attribution: "Richard Bach"
+    // },
     {
-        quote: "You teach best what you most need to learn.",
-        attribution: "Richard Bach"
-    },
-    {
-        quote: "But as things got thinner. . . our hearts got thinner, too. I suppose that kept things in balance.",
+        quote: "But as things got thinner, our hearts got thinner, too. I suppose that kept things in balance.",
         attribution: "The Memory Police"
     },
-    {
-        quote: "The best gift I can give you is one of the things I’ve been hiding",
-        attribution: "The Memory Police"
-    },
+    // {
+    //     quote: "The best gift I can give you is one of the things I’ve been hiding",
+    //     attribution: "The Memory Police"
+    // },
     {
         quote: "Algorithms are opinions embedded in code",
         attribution: "Cathy O'Neil"
@@ -82,8 +82,20 @@ export const QUOTES: Quote[] = [
         quote: "When the residents forget birds and roses, they forget what these things conjure inside them: flight, freedom, extravagance, desire.",
         attribution: "Tolentino - on the Memory Police"
     },
+    // {
+    //     quote: "There are two kinds of arrogance. One where you are unequal to the task and one where your dreams are too big. The former is commonplace stupidity, but the latter is a rare species that is difficult to find.",
+    //     attribution: "Fate zero, Gilgamesh"
+    // },
     {
-        quote: "There are two kinds of arrogance. One where you are unequal to the task and one where your dreams are too big. The former is commonplace stupidity, but the latter is a rare species that is difficult to find.",
-        attribution: "Fate zero, Gilgamesh"
+        quote: "I'm having trouble because I'm a Hollywood hack who never gave real literature a shot",
+        attribution: "Midnight in paris"
+    },
+    {
+        quote: "TONY ‘Mr. Wood, I only got one hour of sleep.’ ED ‘Yeah? Well I got no sleep, and I feel great!’",
+        attribution: "Ed Wood 1994"
+    },
+    {
+        quote: "It’s not always at the top of the mountain. It might be in a crack on the smoothest cliff or somewhere deep in the valley.",
+        attribution: "Housekeeper and the Professor"
     }
 ];
